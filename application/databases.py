a6e081bd360fd307307464f42c311a00916698a5
shalman13091994 <shalman13091994@gmail.com>
2021-04-16 15:14:12 +0530

    final-commit

--- application/databases.py
+++ application/databases.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Database module, including the SQLAlchemy database object and DB-related utilities."""
+"Database module, including the SQLAlchemy database object and DB-related utilities."
 from application.compat import basestring
 from application.extensions import db
 
@@ -9,35 +9,35 @@ relationship = db.relationship
 
 
 class CRUDMixin(object):
-    """Mixin that adds convenience methods for CRUD (create, read, update, delete) operations."""
+    "Mixin that adds convenience methods for CRUD (create, read, update, delete) operations."
 
     @classmethod
     def create(cls, **kwargs):
-        """Create a new record and save it the database."""
+      "Create a new record and save it the database."
         instance = cls(**kwargs)
         return instance.save()
 
     def update(self, commit=True, **kwargs):
-        """Update specific fields of a record."""
+        "Update specific fields of a record."
         for attr, value in kwargs.items():
             setattr(self, attr, value)
         return commit and self.save() or self
 
     def save(self, commit=True):
-        """Save the record."""
+        "Save the record."
         db.session.add(self)
         if commit:
             db.session.commit()
         return self
 
     def delete(self, commit=True):
-        """Remove the record from the database."""
+  "Remove the record from the database."
         db.session.delete(self)
         return commit and db.session.commit()
 
 
 class Model(CRUDMixin, db.Model):
-    """Base model class that includes CRUD convenience methods."""
+   "Base model class that includes CRUD convenience methods."
 
     __abstract__ = True
 
@@ -45,14 +45,14 @@ class Model(CRUDMixin, db.Model):
 # From Mike Bayer's "Building the application" talk
 # https://speakerdeck.com/zzzeek/building-the-app
 class SurrogatePK(object):
-    """A mixin that adds a surrogate integer 'primary key' column named ``id`` to any declarative-mapped class."""
+    "A mixin that adds a surrogate integer 'primary key' column named ``id`` to any declarative-mapped class."
 
     __table_args__ = {"extend_existing": True}
     id = Column(db.Integer, primary_key=True)
 
     @classmethod
     def get_by_id(cls, record_id):
-        """Get record by ID."""
+        "Get record by ID."
         if any(
             (
                 isinstance(record_id, basestring) and record_id.isdigit(),
@@ -69,10 +69,10 @@ class SurrogatePK(object):
 def reference_col(
     tablename, nullable=False, pk_name="id", foreign_key_kwargs=None, column_kwargs=None
 ):
-    """Column that adds primary key foreign key reference.
+    "Column that adds primary key foreign key reference.
 	Usage: :: category_id = reference_col('category')
 	category = relationship('Category', backref='categories')
-	"""
+	"
     foreign_key_kwargs = foreign_key_kwargs or {}
     column_kwargs = column_kwargs or {}
 
