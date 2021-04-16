c9f72c4803f0968c1bf014f24a7f313298286bec
shalman13091994 <shalman13091994@gmail.com>
2021-04-16 15:09:56 +0530

    settings.py

--- application/extensions.py
+++ application/extensions.py
@@ -1,4 +1,4 @@
-"""Extensions module. Each extension is initialized in the application factory located in application.py."""
+"Each extension is initialized in the application factory located in application.py."
 
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
